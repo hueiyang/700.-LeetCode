4297d75cfd77ef9e0ece30288b32ecd0f915ecef
hy-nb <edgejerry@gmail.com>
2019-02-20 01:10:01 +0800

    #26 modified.

--- 26. Remove Duplicates from Sorted Array/26. removeDuplicates.java
+++ 26. Remove Duplicates from Sorted Array/26. removeDuplicates.java
@@ -1,8 +1,8 @@
 class Solution { // Runtime: 6 ms, faster than 97.50%
     public int removeDuplicates(int[] nums) {
         // 判斷不同處的指標, defalut: 1 預設有一個數字
-        int nowFlag = 1;    
-        
+        int nowFlag = 1;
+	
         for(int i = 0 ; i < nums.length-1 ; i++){
             // 只要發現前後不同，就把不同的[i+1]指定到flag所在地
             if(nums[i] != nums[i+1]){
